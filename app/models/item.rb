ed2f2c4bc808b3af0d237abba68036b257ed29ac
Nishikokura Hironobu <nishiko@mindia.jp>
2019-03-16 03:23:50 +0000

    add seeds

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  def self.init
+    [
+      %w(Cloud9 cloud9),
+      %w(GitHub github),
+      %w(TimeCrowd timecrowd),
+      %w(Ruffnote ruffnote),
+      %w(GoogleSpreadsheet ss),
+    ].each do |item_names|
+      Item.create!(name: item_names[0], key: item_names[1])
+    end
+  end
 end
